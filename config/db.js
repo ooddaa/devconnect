57e1978ce8f508103b3fd0c9d5e44489eeaf3004
Dima <dv@kappacoding.co.uk>
2020-08-22 10:52:02 +0100

    pass env for dbuser dbpass

--- config/db.js
+++ config/db.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
 const config = require('config');
 const util = require('util')
-const db = config.get('mongoURI');
+
+let DBUSER, DBPASS;
+if (!process.env.DBUSER) {
+    console.error("No db user specified");
+    process.exit(1);
+} else {
+    DBUSER = process.env.DBUSER
+}
+
+if (!process.env.DBPASS) {
+    console.error("No db pass specified");
+    process.exit(1);
+} else {
+    DBPASS = process.env.DBPASS
+}
+
+const db = config.get('mongoURI').replace(/<user>/, DBUSER).replace(/<pass>/, DBPASS);
 
 const log = (...items) =>
     items.forEach(item =>
@@ -17,7 +33,6 @@ async function connectDB() {
         });
 
         console.log('MongoDB connected\n');
-        // log(cn)
     } catch (error) {
         console.error(error.message);
         process.exit(1)
@@ -25,95 +40,3 @@ async function connectDB() {
 }
 
 module.exports = connectDB
-
-/*
-Mongoose {
-  connections:
-   [ NativeConnection {
-       base: [Circular],
-       collections: {},
-       models: {},
-       config: [Object],
-       replica: false,
-       options: null,
-       otherDbs: [],
-       relatedDbs: {},
-       states: [Object],
-       _readyState: 1,
-       _closeCalled: false,
-       _hasOpened: true,
-       plugins: [],
-       id: 0,
-       _listening: false,
-       _connectionString:
-        'mongodb+srv://oda_user:Qqqwwe!23@oda1.udkui.mongodb.net/test?retryWrites=true&w=majority',
-       _connectionOptions: [Object],
-       client: [MongoClient],
-       '$initialConnection': [Promise],
-       name: 'test',
-       host: 'oda1-shard-00-00.udkui.mongodb.net',
-       port: 27017,
-       user: 'oda_user',
-       pass: 'Qqqwwe!23',
-       db: [Db] } ],
-  models: {},
-  modelSchemas: {},
-  options: { pluralization: true, [Symbol(mongoose:default)]: true },
-  _pluralize: [Function: pluralize],
-  Schema:
-   { [Function: Schema]
-     reserved:
-      [Object: null prototype] {
-        validate: 1,
-        toObject: 1,
-        schema: 1,
-        save: 1,
-        remove: 1,
-        populated: 1,
-        isNew: 1,
-        isModified: 1,
-        init: 1,
-        get: 1,
-        errors: 1,
-        collection: 1,
-        removeListener: 1,
-        on: 1,
-        listeners: 1,
-        emit: 1,
-        prototype: 1 },
-     Types:
-      { String: [Function],
-        Number: [Function],
-        Boolean: [Function],
-        DocumentArray: [Function],
-        Embedded: [Function],
-        Array: [Function],
-        Buffer: [Function],
-        Date: [Function],
-        ObjectId: [Function],
-        Mixed: [Function],
-        Decimal: [Function],
-        Decimal128: [Function],
-        Map: [Function],
-        Oid: [Function],
-        Object: [Function],
-        Bool: [Function],
-        ObjectID: [Function] },
-     ObjectId:
-      { [Function: ObjectId]
-        schemaName: 'ObjectId',
-        defaultOptions: {},
-        get: [Function],
-        set: [Function: set],
-        _checkRequired: [Function],
-        _cast: [Function: castObjectId],
-        cast: [Function: cast],
-        checkRequired: [Function] } },
-  model: [Function],
-  plugins:
-   [ [ [Function], [Object] ],
-     [ [Function], [Object] ],
-     [ [Function], [Object] ],
-     [ [Function], [Object] ],
-     [ [Function: trackTransaction], [Object] ] ] }
-*/

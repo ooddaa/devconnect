23da8e29bebc99b56e9a4f3ec2650f619e683221
Dima <dv@kappacoding.co.uk>
2020-09-16 14:37:05 +0100

    ready for dep 2

--- config/db.js
+++ config/db.js
@@ -1,28 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const util = require('util')
 
-let DBUSER, DBPASS;
-if (!process.env.DBUSER) {
-    console.error("No db user specified");
-    process.exit(1);
-} else {
-    DBUSER = process.env.DBUSER
-}
-
-if (!process.env.DBPASS) {
-    console.error("No db pass specified");
-    process.exit(1);
-} else {
-    DBPASS = process.env.DBPASS
-}
-
-const db = config.get('mongoURI').replace(/<user>/, DBUSER).replace(/<pass>/, DBPASS);
-
-const log = (...items) =>
-    items.forEach(item =>
-        console.log(util.inspect(item, { depth: null, colors: true }))
-    );
+const db = config.get('mongoURI');
 
 async function connectDB() {
     try {
@@ -35,7 +14,7 @@ async function connectDB() {
 
         console.log('MongoDB connected\n');
     } catch (error) {
-        console.error(error.message);
+        console.error('connectDB error:', error.message);
         process.exit(1)
     }
 }
